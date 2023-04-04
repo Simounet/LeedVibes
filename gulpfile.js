a3eed8f691a09bfaa4f268f6c000ae0bb956fe3d
Simounet <contact@simounet.net>
2023-04-04 21:43:08 +0200

    Sass dev dependencies updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const gulp = require('gulp');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('node-sass'));
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
 
@@ -14,7 +14,7 @@ const faviconsPath = 'images/favicons/';
 gulp.task('css', () => {
     return gulp.src('./sass/**/*.scss')
         .pipe(sourcemaps.init())
-        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+        .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
         .pipe(autoprefixer({
             cascade: false
         }))
