90168d387667fd551fa2ff3beeb4fa4b0407ebb8
Simounet <contact@simounet.net>
2025-02-11 22:51:31 +0100

    Better title list touch spacing

--- js/script.js
+++ js/script.js
@@ -563,7 +563,7 @@ function EventObject (event) {
 }
 
 EventObject.prototype = {
-    titleClass: 'js-article-title',
+    titleClass: 'js-article-link',
     articleClass: 'js-article__description',
     contentClass: 'js-leedvibes-article-content',
     readButtonClass: 'js-read-unread',
